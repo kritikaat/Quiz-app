2f7d4b1acf4285e9fbe9c0be66efef395ec25ebc
krutika thakkar <134754200+kritikaat@users.noreply.github.com>
2023-10-30 14:00:15 +0530

    Add files via upload

---
+++ Quiz.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:flutterbasicadv/data/questions.dart';
+import 'package:flutterbasicadv/question_screen.dart';
+import 'package:flutterbasicadv/start_screen.dart';
+import 'package:flutterbasicadv/results_screen.dart';
+
+class Quiz extends StatefulWidget{
+  const Quiz({super.key});//accept and forward name key argument
+
+  @override
+  State<Quiz> createState(){
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz>{//'_' this will make this class private and can only be used in this file
+  final List<String> selectedAnswers = [];
+  var activescreen = 'start-screen';
+
+  void SwitchScreen(){
+    setState(() {
+      activescreen =  'question-screen';
+    });
+  }
+
+  void chooseAnswer(String answer){
+     selectedAnswers.add(answer);//we are not manipulating the string here, we are just adding up the new answers so the string can be final
+     if(selectedAnswers.length == questions.length){
+       setState(() {
+         activescreen = 'results-screen';
+       });
+     }    
+ 
+  }
+
+   @override
+   Widget build(context){
+    Widget screenwidget = StartScreen(SwitchScreen);
+
+    if(activescreen == 'question-screen'){
+             screenwidget =  question_Screen(onSelectAnswer: chooseAnswer,);
+    }
+    
+    if(activescreen == 'results-screen'){
+      screenwidget =  ResultsScreen(chosenAnswers: selectedAnswers,
+      );
+    }
+
+
+     return MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: RadialGradient(
+            colors: [
+              Colors.deepPurpleAccent,
+              Color.fromARGB(255, 137, 96, 252),
+            ],
+          ),
+        ),
+        child: screenwidget
+      ),
+    ),
+   );
+  }
+
+}
---
+++ start_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget{
+  const StartScreen(this.startQuiz,{super.key});
+
+  final void Function() startQuiz;//this is the call for function in switchscreen just name is changed
+
+  @override
+  Widget build(context){
+    return Center(
+      child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                Image.asset('assets/assets/images/quiz-logo.png',
+                    width: 300, 
+                    height: 200,
+                    color:const Color.fromARGB(150, 227, 223, 223),
+                    ),
+
+                const SizedBox(height: 40),
+
+                const Center(
+                  child: Text(
+                    'Learn Flutter The Fun Way!',
+                    style: TextStyle(
+                        color: Color.fromARGB(213, 241, 239, 239),
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+
+                const SizedBox(height: 35),
+
+                OutlinedButton.icon(
+                  onPressed: () {
+                     startQuiz();//when clicked the screen is switched 
+                  },
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.white38,
+                  ),
+                  icon: const Icon(Icons.arrow_right_alt),
+                  label: const Text(
+                    'start Quiz',
+                  ),
+                )
+              ],
+            ),
+    );
+  }
+}
